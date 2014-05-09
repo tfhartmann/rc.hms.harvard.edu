f8042a31db91b34148faafd18ed777501d812526
Nam Pho <Nam_Pho@hms.harvard.edu>
2014-05-09 09:00:50 -0400

    #481286 upgrade bootstrap and new jquery forms

---
+++ docroot/form.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * This is the PHP for the support form
+ */
+
+if (isset($_POST['form_support_email'])) {
+  //if "email" is filled out, send email
+
+  $mailto = "Nam_Pho@hms.harvard.edu"; // test
+  //$mailto = "submit-it-ritg@rt.med.harvard.edu"; // prod actually makes a ticket
+  $email = strip_tags($_POST['form_support_email']);
+  $subject = strip_tags($_POST['form_support_subject']);
+  $first_name = strip_tags($_POST['form_support_first_name']);
+  $last_name = strip_tags($_POST['form_support_last_name']);
+  $group = strip_tags($_POST['form_support_group']);
+
+  $message = "This ticket was submitted via http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . "
+
+-------------------------------------------------------------------------------
+Requestor's contact information:
+
+        $first_name $last_name	<$email>
+        Group:\t\t$group
+-------------------------------------------------------------------------------
+
+";
+
+  $message .= strip_tags($_POST['form_support_message']);
+
+  //print($message);
+
+  // actually mail out the converted form  
+  mail($mailto, $subject, $message, "From:" . $email);
+  
+  echo "
+   	<h1>Submit a New Support Request</h1>
+    	<br />
+	<p>You can close this window; you will get an e-mail with your ticket number shortly.</p>
+	<br />
+	<p>Thank you for using our web form.</p>
+	<br />
+	<p> -- HMS Research Computing </p>
+  ";
+
+  //print_r(array_values(get_defined_vars())); // debug
+}
+
+/**
+ * This is the PHP for the new user form
+ */
+
+if (isset($_POST['form_register_email'])) {
+  //if "email" is filled out, send email
+
+  $mailto = "Nam_Pho@hms.harvard.edu"; // test
+  //$mailto = "submit-it-ritg@rt.med.harvard.edu"; // prod actually makes a ticket
+  $email = strip_tags($_POST['form_register_email']) ;
+  $first_name = strip_tags($_POST['form_register_first_name']);
+  $last_name = strip_tags($_POST['form_register_last_name']);
+  $ecommons = strip_tags($_POST['form_register_ecommons']);
+  $subject = "New Orchestra HPC user: ".$first_name." ".$last_name." (".$ecommons.")";
+  $group = strip_tags($_POST['form_register_group']);
+  $research = strip_tags($_POST['form_register_research']);
+  $storage = strip_tags($_POST['form_register_storage']);
+  $compute = strip_tags($_POST['form_register_compute']);
+  $software = strip_tags($_POST['form_register_software']);
+
+  $message = "This ticket was submitted via http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . "
+
+-------------------------------------------------------------------------------
+Requestor's contact information:
+
+        $first_name $last_name <$email>
+        Group:\t\t$group
+-------------------------------------------------------------------------------
+
+eCommons ID: $ecommons
+
+Research summary: $research
+
+Data storage estimate: $storage
+
+Computational resource usage estimate: $compute
+
+Software requirements: $software
+
+";
+
+  //print($message);
+
+  // actually mail out the converted form  
+  mail($mailto, $subject, $message, "From:" . $email);
+  
+  echo "
+    <h1>Orchestra New User Account Request</h1>
+    <br />
+    <p>You can close this window; you will get an e-mail with your ticket number shortly.</p>
+    <br />
+    <p>Thank you for using our web form.</p>
+    <br />
+    <p> -- HMS Research Computing </p>
+  ";
+
+  //print_r(array_values(get_defined_vars())); // debug
+}
+
+?>
---
+++ docroot/js/custom.js
@@ -0,0 +1,135 @@
+/*!
+ * Custom Javascript for HMS Research Computing @ rc.hms.harvard.edu
+ * originally by: Nam Pho - April 28, 2014
+ * last updated: Nam Pho - May 6, 2014
+ */
+
+$(document).ready(function(){
+    // rotating menu
+    $('.carousel').carousel();
+
+    console.log("hello jQuery world");
+
+    // all client side form checks below
+    $('input[type=text][class=form-control]').keyup(notBlank);
+    $('textarea').keyup(notBlank);
+
+    $('#ecommons').keyup(validateEC);
+    $('input[name=form_support_email]').keyup(validateEmail);
+    $('input[name=form_register_email]').keyup(validateEmail);
+});
+
+$(function() {
+    $('button#form_support_submit').click(function() {
+        // validate form first
+        console.log("the support submit button works");
+
+	if (1) {
+	  // now send using ajax
+	  $.ajax({
+	    type: 'POST',
+            url: '/test/form.php',
+            data: $('#form_support_form').serialize(),
+            success: function(resp) {
+		$('#form_support_div').modal('hide');
+		$('#form_ok').modal('show');
+		console.log(resp);
+            }
+          });
+	}
+    });
+});
+
+$(function() {
+    $('button#form_register_submit').click(function() {
+        // validate form first
+        console.log("the register submit button works");
+
+	if (1) {
+	  // now send using ajax
+	  $.ajax({
+	    type: 'POST',
+            url: '/test/form.php',
+            data: $('#form_register_form').serialize(),
+            success: function(resp) {
+		$('#form_register_div').modal('hide');
+		$('#form_ok').modal('show');
+		console.log(resp);
+            }
+          });
+	}
+    });
+});
+
+function scrollToElement( target ) {
+    var topoffset = 30;
+    var speed = 800;
+    var destination = jQuery( target ).offset().top - topoffset;
+
+    jQuery( 'html:not(:animated),body:not(:animated)' ).animate( { scrollTop: destination}, speed, function() {
+        window.location.hash = target;
+    });
+
+    return false;
+}
+      
+function notBlank() {
+    // checks the field if blank
+    var field = $(this).val().toString();
+
+    if(field.length > 0) {
+        $(this).parents('div.form-group').attr("class", "form-group has-success has-feedback");
+        $(this).next('.form-control-feedback').attr("class", "glyphicon form-control-feedback glyphicon-ok");
+
+        return true;
+    } else {
+        $(this).parents('div.form-group').attr("class", "form-group has-warning has-feedback");
+        $(this).next('.form-control-feedback').attr("class", "glyphicon form-control-feedback glyphicon-warning-sign");
+
+        return false;
+    }
+}
+
+function validateEC() {
+    // validates ecommons
+    var ec = $(this).val().toString();
+
+    if(ec.length > 0) {
+        var re = /[A-Za-z]+\d+/;
+
+        if(!re.test(ec)) {
+            $(this).parents('div.form-group').attr("class", "form-group has-error has-feedback");
+            $(this).next('.form-control-feedback').attr("class", "glyphicon form-control-feedback glyphicon-remove");
+        } else {
+            $(this).parents('div.form-group').attr("class", "form-group has-success has-feedback");
+            $(this).next('.form-control-feedback').attr("class", "glyphicon form-control-feedback glyphicon-ok");
+           
+            return true;
+        }
+    } else {
+        $(this).parents('div.form-group').attr("class", "form-group has-warning has-feedback");
+        $(this).next('.form-control-feedback').attr("class", "glyphicon form-control-feedback glyphicon-warning-sign");
+    }
+
+    return false;
+}
+
+function validateEmail() {
+    // validates e-mail
+    var email = $(this).val().toString();
+
+    var atpos = email.indexOf("@");
+    var dotpos = email.lastIndexOf(".");
+
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=email.length) {
+        $(this).parents('div.form-group').attr("class", "form-group has-error has-feedback");
+	$(this).next('.form-control-feedback').attr("class", "glyphicon form-control-feedback glyphicon-remove");
+    } else {    
+        $(this).parents('div.form-group').attr("class", "form-group has-success has-feedback");
+        $(this).next('.form-control-feedback').attr("class", "glyphicon form-control-feedback glyphicon-ok");
+
+	return true;
+    }
+
+    return false;
+}
